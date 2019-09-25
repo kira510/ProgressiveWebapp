bb700b11d28b151d59cfa4cef4afdc5350a0d7b3
kira3691 <kiran.k@accion.labs.com>
2019-09-25 15:22:23 +0530

    learn fetch/ajax/promises, add custom promise and fetch so they are supported in all browsers

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,10 +1,16 @@
 var deferredPrompt;
 
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js', { scope: '/' })
     .then(() => {
       navigator.geolocation.getCurrentPosition((position) => {console.log(position)});
+    }).catch((err) => {
+      console.log(err);
     });
 }
 
@@ -14,3 +20,51 @@ window.addEventListener('beforeinstallprompt', (event) => {
   deferredPrompt = event;
   return false;
 });
+
+fetch('http://httpbin.org/ip')
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
+fetch('http://httpbin.org/post', {
+    method: "POST",
+    headers: {
+      'Content-type': 'application/json',
+      'Accept': 'application/json'
+    },
+    mode: 'cors',
+    body: JSON.stringify({message: 'Does this work?'})
+  })
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
+/**
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://httpbin.org/ip');
+xhr.responseType = 'json';
+
+xhr.onload = () => {
+  console.log(xhr.response);
+}
+
+xhr.onerror = () => {
+  console.log('Error!')
+}
+
+xhr.send();
+*/
